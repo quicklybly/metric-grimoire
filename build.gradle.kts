6dcc7f7314d892faaccbafe4d9d4786b1bcfb332
quicklybly <quickly.tawer@gmail.com>
2024-11-30 22:55:10 +0300

    feat: add detekt

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
-    kotlin("jvm") version "1.9.25"
-    kotlin("plugin.spring") version "1.9.25"
+    kotlin("jvm") version "1.9.23"
+    kotlin("plugin.spring") version "1.9.23"
+    id("io.gitlab.arturbosch.detekt") version("1.23.6")
     id("org.springframework.boot") version "3.3.5"
     id("io.spring.dependency-management") version "1.1.6"
 }
